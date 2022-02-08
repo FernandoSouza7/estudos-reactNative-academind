235baca8b83ba1f9f9b629cf51fa231fb899b420
matheus-fernando <matheus.fernando.coder@gmail.com>
2022-02-08 19:49:54 -0300

    working on flex-box

--- App.js
+++ App.js
@@ -1,20 +1,48 @@
 import React from 'react';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import { Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={{ padding: 50 }}>
+    <View style={{
+      padding: 50,
+      flexDirection: 'row',
+      width: '80%',
+      height: 300,
+      justifyContent: 'space-around',
+      alignItems: 'center',
+      }}>
 
-      <View>
-        <TextInput
-          placeholder='Your goals'
-          style={{ borderColor: 'black' , borderWidth: 1, padding: 10 }}
-        />
-        <Button title="Add"/>
+      <View
+       style={{
+         backgroundColor: 'red',
+         flex: 1,
+         justifyContent: 'center',
+         alignItems: 'center',
+       }}
+      >
+        <Text>1</Text>
       </View>
 
-      <View>
-      
+      <View
+      style={{
+        backgroundColor: 'blue',
+        flex: 2,
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+      >  
+      <Text>2</Text>
+
+      </View>
+
+      <View
+      style={{
+        backgroundColor: 'green',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+      >
+        <Text>3</Text>
       </View>
        
     </View>
